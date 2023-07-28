61afc4682a221123b26120a2f76eadc08f9b659e
toxirjon96 <tatu.toxirjon@gmail.com>
2023-07-28 09:54:12 +0500

    all methods used

--- lib/src/logic/word_logic.dart
+++ lib/src/logic/word_logic.dart
@@ -4,11 +4,17 @@ class WordLogic {
   const WordLogic();
 
   void call() {
-    String word = stdin.readLineSync() ?? "";
+    String text = stdin.readLineSync() ?? "";
 
     try {
-      Words words = Words(word: word);
-      print(words.word);
+      Words w1 = Words(word: text);
+      Words w2 = w1.copyWith(word: "leetcode");
+      print(w1.word);
+      print(w1.reverseVowels());
+      print(w1.hashCode);
+      print(w1);
+      print(w1 == w2);
+      print(w2.reverseVowels());
     } on EmptyStringException catch (e) {
       print(e.message);
     } on LowerCaseFormatException catch (e) {
